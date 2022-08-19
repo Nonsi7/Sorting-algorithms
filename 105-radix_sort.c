2cdf0bf97e076ba57111c5beed776d59b2785e28
Nonsi <debockannaline@gmail.com>
2022-08-19 14:12:39 +0200

    Update 105-radix_sort.c

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -1,78 +1,73 @@
 #include "sort.h"
 
 /**
- * get_digit - gets a digit from a number
- * @number: the integer
- * @digit: the digit position to get
- *
- * Return: the digit value at given position
-**/
-int get_digit(long number, int digit)
-{
-	long i = 0L, pow = 1L, ret;
+* findmax - Finds the maximum value in an array
+* @array: array to find max value of
+* @size: Size of array
+* Return: Largest value
+*/
 
-	for (i = 0; i < digit; i++)
-		pow *= 10L;
-	ret = ((number / pow) % 10);
-	return (ret);
+int findmax(int *array, size_t size)
+{
+	int i, max = 0;
+
+	for (i = 0; i < (int)size; i++)
+	{
+		if (max < array[i])
+			max = array[i];
+	}
+	return (max);
 }
 
 /**
- * radix_pass - sorts by radix
- * @array: the integer array to sort
- * @size: the size of the array
- * @digit: the current digit to check
- * @new_array: target array of same size
- *
- * Return: void.
- */
-int radix_pass(int *array, ssize_t size, int digit, int *new_array)
-{
-	ssize_t i;
-	int buckets[10] = {0};
+* radix_sort - Sorts an array using radix sort algo
+* @array: Array to sort
+* @size: size of array
+*/
 
-	for (i = 0; i < size; i++)
-		buckets[get_digit(array[i], digit)]++;
-	for (i = 1; i <= 9; i++)
-		buckets[i] += buckets[i - 1];
-	for (i = size - 1; i > -1; i--)
-		new_array[buckets[get_digit(array[i], digit)]-- - 1] = array[i];
-	return (1);
-}
-
-/**
- * radix_sort - sorts by radix
- * @size: the size of the array
- * @array: the integer array to sort
- *
- * Return: the gap size
- */
 void radix_sort(int *array, size_t size)
 {
-	int *old_array, *new_array, *temp_ptr, *ptr, max = 0;
-	size_t i, sd = 1;
+	int m, pos, *out, *ca;
 
-	if (!array || size < 2)
+	if (array == NULL || size < 2)
 		return;
+	m = findmax(array, size);
+	out = malloc(sizeof(int) * (int)size);
+	ca = malloc(sizeof(int) * (10));
+	if (ca == NULL || out == NULL)
+		return;
+	for (pos = 1; m / pos > 0; pos *= 10)
+		counting_sort_r(array, size, pos, out, ca), print_array(array, size);
+	free(out);
+	free(ca);
+}
 
-	for (i = 0; i < size; i++)
-		if (array[i] > max)
-			max = array[i];
-	while (max /= 10)
-		sd++;
-	old_array = array;
-	new_array = ptr = malloc(sizeof(int) * size);
-	if (!new_array)
+/**
+* counting_sort_r - sorts array using counting algorithm
+* @array: Array to sort
+* @size: Size of array
+* @pos: Digit position value
+* @out: Temp output array
+* @ca: Count array
+*/
+
+void counting_sort_r(int *array, size_t size, int pos, int *out, int *ca)
+{
+	int i;
+
+	if (array == NULL || size < 2)
 		return;
-	for (i = 0; i < sd; i++)
+	for (i = 0; i < 10; i++)
+		ca[i] = 0;
+	for (i = 0; i < (int)size; i++)
+		ca[((array[i] / pos) % 10)] += 1;
+	for (i = 0; i < 10; i++)
+		ca[i] += ca[i - 1];
+	for (i = size - 1; i >= 0; i--)
 	{
-		radix_pass(old_array, (ssize_t)size, i, new_array);
-		temp_ptr = old_array;
-		old_array = new_array;
-		new_array = temp_ptr;
-		print_array(old_array, size);
+		out[ca[((array[i] / pos) % 10)] - 1] = array[i];
+		ca[((array[i] / pos) % 10)] -= 1;
 	}
-	for (i = 0; i < size; i++)
-		array[i] = old_array[i];
-	free(ptr);
+	for (i = 0; i < (int)size; i++)
+		array[i] = out[i];
 }
